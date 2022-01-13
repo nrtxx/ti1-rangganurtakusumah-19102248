fb7cecca3ce2bc183dad41e633ee2814255ae5ef
nrtxx <rangganurtakusumah@gmail.com>
2022-01-14 02:45:34 +0700

    student controller

--- practice_laravel/laravel/app/Http/Controllers/StudentController.php
+++ practice_laravel/laravel/app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Student;
 use Illuminate\Http\Request;
+use File;
 
 class StudentController extends Controller
 {
@@ -18,6 +19,7 @@ class StudentController extends Controller
             'jenis_kelamin' => 'required|in:P,L',
             'jurusan' => 'required',
             'alamat' => '',
+            'image' => 'required|file|image|max:1000',
         ]);
         $mahasiswa = new Student();
         $mahasiswa->nim = $validateData['nim'];
@@ -25,6 +27,12 @@ class StudentController extends Controller
         $mahasiswa->gender = $validateData['jenis_kelamin'];
         $mahasiswa->departement = $validateData['jurusan'];
         $mahasiswa->address = $validateData['alamat'];
+        if($request->hasFile('image')) {
+            $extFile = $request->image->getClientOriginalExtension();
+            $namaFile = 'user-'.time().".".$extFile;
+            $path = $request->image->move('assets/images',$namaFile);
+            $mahasiswa->image = $path;
+        }
         $mahasiswa->save();
         $request->session()->flash('pesan','Penambahan data berhasil');
         return redirect()->route('student.index');
@@ -52,18 +60,27 @@ class StudentController extends Controller
             'jenis_kelamin' => 'required|in:P,L',
             'jurusan' => 'required',
             'alamat' => '',
+            'image' => 'required|file|image|max:1000',
             ]);
         $student->nim = $validateData['nim'];
         $student->name = $validateData['nama'];
         $student->gender = $validateData['jenis_kelamin'];
         $student->departement = $validateData['jurusan'];
         $student->address = $validateData['alamat'];
+        if($request->hasFile('image')) {
+            $extFile = $request->image->getClientOriginalExtension();
+            $namaFile = 'user-'.time().".".$extFile;
+            File::delete($student->image);
+            $path = $request->image->move('assets/images',$namaFile);
+            $student->image = $path;
+        }
         $student->save();
         $request->session()->flash('pesan','Perubahan data berhasil');
         return redirect()->route('student.show',['student' => $student->id]);
     }
 
     public function destroy(Request $request, Student $student) {
+        File::delete($student->image);
         $student->delete();
         $request->session()->flash('pesan', 'Hapus data berhasil');
         return redirect()->route('student.index');
